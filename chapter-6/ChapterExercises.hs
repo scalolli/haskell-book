1850d4e8a8b6f0114511fa243e14f26cc820e654
Basavaraj Kalloli <basavaraj.kalloli@gmail.com>
2018-03-08 08:10:13 +0000

    completed chapter exercises

--- chapter-6/ChapterExercises.hs
+++ chapter-6/ChapterExercises.hs
@@ -1,5 +1,7 @@
 module ChapterExercises where
 
+import Data.List
+
 data Person = Person Bool 
     deriving Show
 printPerson :: Person -> IO ()
@@ -39,4 +41,49 @@ equalityForAll p p' = p == p'
 
 comparePapus :: Papu -> Papu -> Bool
 comparePapus p p' = p > p'
+
+
+i :: (Num a) => a
+i = 1
+
+f :: RealFrac a => a
+f = 1.0
+
+
+freud :: Ord a => a -> a
+freud x = x
     
+
+freud' :: Int -> Int
+freud' x = x    
+
+
+myX = 1 :: Int
+sigmund :: Int -> Int 
+sigmund x = myX
+
+sigmund' :: Int -> Int
+sigmund' x = myX
+
+
+jung :: [Int] -> Int
+jung xs = head (sort xs)
+
+
+young :: Ord a => [a] -> a
+young xs = head (sort xs)
+
+mySort :: [Char] -> [Char]
+mySort xs = sort xs
+
+signifier :: [Char] -> Char
+signifier xs = head (mySort xs)
+
+chk :: Eq b => (a -> b) -> a -> b -> Bool
+chk f a b = f a == b
+
+resOfChk = chk (\x -> x-1) 3 2                
+
+
+arith :: Num b => (a -> b) -> Integer -> a -> b
+arith f i a = (f a) + fromInteger i
