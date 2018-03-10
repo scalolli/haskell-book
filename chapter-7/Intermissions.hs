21860f0f5bfb6b8c2ac33d71eb3ee3684a6c206a
Basavaraj Kalloli <basavaraj.kalloli@gmail.com>
2018-03-10 09:00:15 +0000

    pattern matching

--- chapter-7/Intermissions.hs
+++ chapter-7/Intermissions.hs
@@ -21,3 +21,25 @@ addOneIfOdd n =
 addFive = \x -> \y -> (if x > y then y else x) + 5
 
 myFlip f x y = f y x
+
+isItTwo :: Integer -> Bool
+isItTwo 2 = True
+isItTwo _ = False
+
+
+newtype Username = 
+    Username String
+
+newtype AccountNumber =
+    AccountNumber Integer
+
+data User = 
+    UnregisteredUser | RegisteredUser Username AccountNumber
+
+printUser :: User -> IO ()
+printUser UnregisteredUser = 
+    putStrLn "UnregisteredUser"
+printUser (RegisteredUser 
+            (Username name)
+            (AccountNumber accountNumber)) = 
+            putStrLn $ name ++ " " ++ show accountNumber             
