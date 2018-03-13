6fcedf669d98f8ac8adfd6df7ce445c2e9917953
Basavaraj Kalloli <basavaraj.kalloli@gmail.com>
2018-03-13 07:07:16 +0000

    completed chapter7 exercises

--- chapter-7/Exercises.hs
+++ chapter-7/Exercises.hs
@@ -1,10 +1,12 @@
 module Exercises where
 
 tensDigit :: Integral a => a -> a
+hundresDigit :: Integral a => a -> a
 
+xPlaceDigit :: (Integral a) => a -> a -> a
 xPlaceDigit y x = r
-    where (quot, remain) = x `divMod` y
-          divMod' = divMod quot
+    where (quot', _) = x `divMod` y
+          divMod' = divMod quot'
           d = snd . divMod'
           r = d y
 
@@ -12,6 +14,22 @@ tensDigit = xPlaceDigit 10
 hundresDigit = xPlaceDigit 100
 
 
+foldBool :: a -> a -> Bool -> a
+-- foldBool x _ False = x
+-- foldBool _ y True = y
+
+-- foldBool x y z =
+--       case z of
+--          False -> x
+--          True -> y
+
+foldBool x y z
+    | z == False = x
+    | otherwise = y
+
+
+g :: (a -> b) -> (a, c) -> (b, c)
+g f (a, c) = (f a, c)
 
 
 
