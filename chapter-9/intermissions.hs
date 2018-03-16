74713c290db9bedddbe6f36035e28c7fe2134aa7
Basavaraj Kalloli <basavaraj.kalloli@gmail.com>
2018-03-16 07:19:18 +0000

    chapter-9 Fearful symmetry

--- chapter-9/intermissions.hs
+++ chapter-9/intermissions.hs
@@ -12,3 +12,34 @@ enumFromTo' :: (Enum a, Ord a) => a -> a -> [a]
 enumFromTo' a b
     | a <= b = a : (enumFromTo' (succ a) b)
     | otherwise = []
+
+
+myWords' :: String -> Char -> [String]
+myWords' [] _ = []
+myWords' sentence sep = word : (myWords' nextPhrase sep)
+            where
+                nextPhrase = dropWhile (== sep) (dropWhile (/= sep) sentence)
+                word = (takeWhile (/= sep) sentence)
+
+myWords :: String -> [String]
+myWords [] = []
+myWords sentence = myWords' sentence ' '
+
+myLines :: String -> [String]
+myLines a = myWords' a '\n'
+
+firstSen = "Tyger Tyger, burning bright\n"
+secondSen = "In the forests of the night\n"
+thirdSen = "What immortal hand or eye\n"
+fourthSen = "Could frame thy fearful symmetry?"
+sentences = firstSen ++ secondSen ++ thirdSen ++ fourthSen
+
+shouldEqual =
+     [ "Tyger Tyger, burning bright"
+     , "In the forests of the night"
+     , "What immortal hand or eye"
+     , "Could frame thy fearful symmetry?"
+     ]
+
+main :: IO ()
+main = print $ "Are they equal " ++ show (myLines sentences == shouldEqual)
