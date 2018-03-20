92a62a5cfabf46f28cbd40f35ee8f27ef0421e87
Basavaraj Kalloli <basavaraj.kalloli@gmail.com>
2018-03-20 07:21:16 +0000

    chapter-9 zip chapter exercises

--- chapter-9/Exercises.hs
+++ chapter-9/Exercises.hs
@@ -12,3 +12,29 @@ capitalizeHead :: [Char] -> Char
 capitalizeHead xs = r xs
         where
             r = toUpper . head
+
+myOr :: [Bool] -> Bool
+myOr [] = False
+myOr (x:xs) =
+        if(x)
+         then True
+         else myOr xs
+
+
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny f [] = False
+myAny f (x:xs) =
+        if(f x)
+        then True
+        else myAny f xs
+
+
+myElem :: Eq a => (a -> a -> Bool) -> a -> [a] -> Bool
+myElem f a [] = False
+myElem f a (x:xs) =
+    if(f a x)
+    then True
+    else myElem f a xs
+
+myElemUsingAny :: Eq a => a -> [a] -> Bool
+myElemUsingAny = myElem (==)
