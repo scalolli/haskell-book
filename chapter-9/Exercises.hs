ebee96da982461f9192ed28cb2b1c7a3bca27822
Basavaraj Kalloli <basavaraj.kalloli@gmail.com>
2018-03-21 07:32:28 +0000

    chapter-9 maximum and minimum

--- chapter-9/Exercises.hs
+++ chapter-9/Exercises.hs
@@ -55,3 +55,25 @@ squishMap f (x : xs) = (f x) ++ squishMap f xs
 
 squishAgain :: [[a]] -> [a]
 squishAgain xs = squishMap (\x -> x) xs
+
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy _ [] = error "Max of empty list"
+myMaximumBy _ [x] = x
+myMaximumBy f (x:xs) = myMaximumBy' f xs x
+
+myMaximumBy' :: (a -> a -> Ordering) -> [a] -> a -> a
+myMaximumBy' f [] acc = acc
+myMaximumBy' f (x:xs) acc = myMaximumBy' f xs maxValue
+        where maxValue = if (f x acc >= GT) then x else acc
+
+
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy _ [] = error "Minimum of empty list"
+myMinimumBy _ [x] = x
+myMinimumBy f (x :xs) = go xs x
+    where go xs' acc =
+            case xs' of
+                [] -> acc
+                (y:ys) -> go ys minValue
+                    where
+                        minValue = if (f y acc <= LT) then y else acc
