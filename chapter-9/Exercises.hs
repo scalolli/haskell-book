3544aa132c0b73594101cf558583c44b04e25b9a
Basavaraj Kalloli <basavaraj.kalloli@springernature.com>
2018-03-22 06:25:56 +0000

    Basu - refactored myMin and myMax

--- chapter-9/Exercises.hs
+++ chapter-9/Exercises.hs
@@ -57,23 +57,19 @@ squishAgain :: [[a]] -> [a]
 squishAgain xs = squishMap (\x -> x) xs
 
 myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
-myMaximumBy _ [] = error "Max of empty list"
+myMaximumBy _ [] = error "Minimum of empty list"
 myMaximumBy _ [x] = x
-myMaximumBy f (x:xs) = myMaximumBy' f xs x
-
-myMaximumBy' :: (a -> a -> Ordering) -> [a] -> a -> a
-myMaximumBy' f [] acc = acc
-myMaximumBy' f (x:xs) acc = myMaximumBy' f xs maxValue
-        where maxValue = if (f x acc >= GT) then x else acc
-
+myMaximumBy f (x:xs) = findElemByComparisonFunction f (>= GT) xs x
 
 myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
 myMinimumBy _ [] = error "Minimum of empty list"
 myMinimumBy _ [x] = x
-myMinimumBy f (x :xs) = go xs x
-    where go xs' acc =
-            case xs' of
-                [] -> acc
-                (y:ys) -> go ys minValue
-                    where
-                        minValue = if (f y acc <= LT) then y else acc
+myMinimumBy f (x :xs) = findElemByComparisonFunction f (<= LT) xs x
+
+findElemByComparisonFunction :: (a -> a -> Ordering) -> (Ordering -> Bool) -> [a] -> a -> a                        
+findElemByComparisonFunction f g xs acc = 
+    case xs of
+        [] -> acc
+        (y:ys) -> findElemByComparisonFunction f g ys minValue
+            where
+                minValue = if (g $ f y acc) then y else acc
