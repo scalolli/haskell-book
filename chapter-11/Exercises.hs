57fedc1ef69416dfbd2b8e7757d2575ec3f90193
scalolli <basavaraj.kalloli@gmail.com>
2018-03-28 11:09:09 +0100

    Basu - as pattern exercises

--- chapter-11/Exercises.hs
+++ chapter-11/Exercises.hs
@@ -3,3 +3,9 @@ module Exercises where
     doubleUp :: [a] -> [a]
     doubleUp [] = []
     doubleUp xs@(x:_) = x:xs
+
+    isSubsetOf :: (Eq a)
+                => [a]
+                -> [a]
+                -> Bool
+    isSubsetOf = undefined
