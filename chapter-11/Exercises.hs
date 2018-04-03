362e663e9074f6b082b0e749d14e545238a3d19e
scalolli <basavarajkalloli@gmail.com>
2018-04-03 07:34:28 +0100

    more exercises

--- chapter-11/Exercises.hs
+++ chapter-11/Exercises.hs
@@ -1,5 +1,7 @@
 module Exercises where
 
+    import Data.Char
+
     doubleUp :: [a] -> [a]
     doubleUp [] = []
     doubleUp xs@(x:_) = x:xs
@@ -14,3 +16,33 @@ module Exercises where
         if (y == x) 
             then isSubseqOf ys xs
              else isSubseqOf l xs
+
+    
+    capitalizeWords :: String -> [(String, String)]
+    capitalizeWords xs = map go (splitWords xs [] [])
+            where go l@(_:_) = (l, capitalizeWord l) 
+
+    splitWords :: String -> String -> [String] -> [String]
+    splitWords [] word acc = acc ++ [word]
+    splitWords (x:xs) word acc = 
+        if(x == ' ')
+        then
+            splitWords xs [] (acc ++ [word])
+        else 
+            splitWords xs (word ++ [x]) acc
+
+    capitalizeWord :: String -> String
+    capitalizeWord [] = []
+    capitalizeWord (x:xs) = [toUpper x] ++ xs
+
+    alphabets = ['a'..'z'] ++ ['A'..'Z']
+
+    splitPara :: String -> Bool -> String -> String
+    splitPara [] _ acc = acc
+    splitPara (x:xs) shouldCapitalize acc = 
+        if(x == '.') 
+            then 
+                splitPara xs True (acc ++ [x])
+            else if (x `elem` alphabets && shouldCapitalize)
+                then splitPara xs False (acc ++ [toUpper x])
+                else splitPara xs shouldCapitalize (acc ++ [x]) 
