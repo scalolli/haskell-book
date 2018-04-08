a88e3ee61483b57a570ec0ed87a462bc2f37ea13
scalolli <basavarajkalloli@gmail.com>
2018-04-08 11:12:07 +0100

    Huttons Razer

---
+++ chapter-11/HuttonsRazer.hs
@@ -0,0 +1,12 @@
+module HuttonsRazer where
+
+    data Expr = Lit Integer | Add Expr Expr
+
+    eval :: Expr -> Integer
+    eval (Lit x) = x
+    eval (Add a b) = (eval a) + (eval b)
+
+    printExpr :: Expr -> String
+    printExpr (Lit x) = show x
+    printExpr (Add a b) = (printExpr a) ++ " + "++  (printExpr b)
+
