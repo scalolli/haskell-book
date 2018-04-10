ffb6f9ec14c04d6777f093eba2034b5a98aa6d2c
Basavaraj Kalloli <basavaraj.kalloli@gmail.com>
2018-04-10 06:10:31 +0100

    implemented natural numbers

--- chapter-12/Exercises.hs
+++ chapter-12/Exercises.hs
@@ -61,3 +61,22 @@ module Exercises where
 
 
 
+    data Nat =
+        Zero | Succ Nat deriving (Eq, Show)
+
+    natToInteger :: Nat -> Integer
+    natToInteger Zero = 0
+    natToInteger (Succ a) = 1 + (natToInteger a)
+
+    integerToNat :: Integer -> Maybe Nat
+    integerToNat a =
+        if(a < 0)
+           then Nothing
+           else Just (go a)
+            where
+                go 0 = Zero
+                go num = Succ (go (num - 1))
+
+
+
+
