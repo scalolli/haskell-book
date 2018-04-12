150b994d10ea132b5f68e867089c1aae12c2f75e
scalolli <basavarajkalloli@gmail.com>
2018-04-12 06:11:08 +0100

    fun with custom show instance for binary trees

--- chapter-12/BinaryTrees.hs
+++ chapter-12/BinaryTrees.hs
@@ -1,7 +1,12 @@
 module BinaryTrees where
 
     data BinaryTree a = Node (BinaryTree a) a (BinaryTree a) | Leaf
-            deriving (Eq, Show, Ord)
+            deriving (Eq, Ord)
+
+    instance Show a => Show (BinaryTree a) where
+        show Leaf = ""
+        show (Node left b right) =
+            show b ++ "*" ++ show left ++ "*" ++ show right
 
     unfold :: (a -> Maybe (a, b, a))
            -> a
@@ -12,4 +17,4 @@ module BinaryTrees where
                 Nothing -> Leaf
 
     treeBuild :: Integer -> BinaryTree Integer
-    treeBuild n = unfold (\a -> if(a > n) then Nothing else Just(a + 1, a, a + 1)) 0
+    treeBuild n = unfold (\a -> if(a >= n) then Nothing else Just(a + 1, a, a + 1)) 0
