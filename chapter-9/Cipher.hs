664cf18150380c9a30a5827b710514a9d661b4a6
scalolli <basavarajkalloli@gmail.com>
2018-04-13 13:36:06 +0100

    modified to work with user input

--- chapter-9/Cipher.hs
+++ chapter-9/Cipher.hs
@@ -1,17 +1,23 @@
 module Cipher where
 
-import Data.Char
+  import Data.Char
 
-encode :: [Char] -> [Char]
-encode xs = map rshift xs
+  encode :: [Char] -> [Char]
+  encode xs = map rshift xs
 
-shift :: Int -> Char -> Char
-shift numOfChars x
-    | isLower x = chr $ ord 'a' + (mod (ord x - ord 'a' + numOfChars) 26)
-    | otherwise = chr $ ord 'A' + (mod (ord x - ord 'A' + numOfChars) 26)
+  shift :: Int -> Char -> Char
+  shift numOfChars x
+      | isLower x = chr $ ord 'a' + (mod (ord x - ord 'a' + numOfChars) 26)
+      | otherwise = chr $ ord 'A' + (mod (ord x - ord 'A' + numOfChars) 26)
 
-rshift = shift 3
-lshift = shift (-3)
+  rshift = shift 3
+  lshift = shift (-3)
 
-decode :: [Char] -> [Char]
-decode xs = map lshift xs
+  decode :: [Char] -> [Char]
+  decode xs = map lshift xs
+
+  main :: IO ()
+  main = do
+    putStrLn "Enter word to encode: "
+    word <- getLine
+    putStrLn $ "Ceaser cipher is: " ++ encode word
