169435f7c8139da62382c2b2f2f19f491d76159d
Basavaraj Kalloli <basavaraj.kalloli@gmail.com>
2018-04-17 07:18:38 +0100

    added hspec discover to look for specs

---
+++ test/Chapter8/WordNumberSpec.hs
@@ -0,0 +1,31 @@
+module Chapter8.WordNumberSpec where
+
+  import Chapter8.WordNumber
+  import Test.Hspec
+
+  main :: IO ()
+  main = hspec spec
+
+  spec :: Spec
+  spec = do
+    describe "Prelude.read" $ do
+      it "returns zero for 0" $ do
+        digitToWord 0 `shouldBe` "zero"
+
+      it "returns one for 1" $ do
+        digitToWord 1 `shouldBe` "one"
+
+    describe "digits" $ do
+      it "returns [1] for 1" $ do
+        digits 1 `shouldBe` [1]
+
+      it "returns [1, 0, 0]" $ do
+        digits 100 `shouldBe` [1, 0, 0]
+
+    describe "wordNumber" $ do
+      it "one-zero-zero given 100" $ do
+        wordNumber 100 `shouldBe` "one-zero-zero"
+
+      it "nine-zero-zero-one for 9001" $ do
+        wordNumber 9001 `shouldBe` "nine-zero-zero-one"
+
