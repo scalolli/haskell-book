321fe6d3948471c5133b35a6738c8fb5d91f7e54
Basavaraj Kalloli <basavaraj.kalloli@gmail.com>
2018-04-19 06:23:06 +0100

    some more quick check

--- test/Chapter14/QuickCheckWithoutHspec.hs
+++ test/Chapter14/QuickCheckWithoutHspec.hs
@@ -5,9 +5,32 @@ module Chapter14.QuickCheckWithoutHspec where
   main :: IO ()
   main = do
     quickCheck property_foldr
+    quickCheck property_foldrConcat
+--     quickCheck property_take
+    quickCheck property_readShowAndBack
+    quickCheck property_square
+
 
   property_foldr :: [Int] -> Bool
   property_foldr xs = (foldr (:) [] xs) == (xs ++ [])
+
+  property_foldrConcat :: [[Int]] -> Bool
+  property_foldrConcat xs = (foldr (++) [] xs) == concat xs
+
+  property_take :: Int -> [Int] -> Bool
+  property_take n xs = (length (take n xs)) == n
+
+  property_readShowAndBack :: Int -> Bool
+  property_readShowAndBack x = (read (show x)) == x
+
+  square :: Float -> Float
+  square x = x * x
+
+  squareIdentity :: Float -> Float
+  squareIdentity = square . sqrt
+
+  property_square :: Float -> Bool
+  property_square x = (squareIdentity x) == x
 
 
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -12,6 +12,3 @@ module Spec where
     hspec Chapter14.QuickCheckExercisesSpec.spec
     hspec Chapter8.WordNumberSpec.spec
     Chapter14.QuickCheckWithoutHspec.main
-
-  property_foldr :: [Int] -> Bool
-  property_foldr xs = (foldr (:) [] xs) == (xs ++ [])
