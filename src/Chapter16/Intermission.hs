5c7b4adf4d7134c3cb934eacf7334717ffb296d8
scalolli <basavarajkalloli@gmail.com>
2018-04-26 07:23:12 +0100

    functors for common types

--- src/Chapter16/Intermission.hs
+++ src/Chapter16/Intermission.hs
@@ -33,11 +33,75 @@ module Chapter16.Intermission where
   type IntInt = Fun Int Int
 
   fcompose' :: [Int] -> IntInt -> IntInt -> Bool
-  fcompose' x (Fun _ f) (Fun _ g) = ((fmap f . fmap g) x) == (fmap (f . g) x)
+  fcompose' x (Fun _ f) (Fun _ g) = functorCompose f g x
+
+-- Functor for Identity
+  newtype Identity a = Identity a deriving (Eq, Show)
+
+  instance Functor Identity where
+    fmap f (Identity a) = Identity (f a)
+
+  instance (Arbitrary a) => Arbitrary (Identity a) where
+    arbitrary = do
+      a <- arbitrary
+      return (Identity a)
+
+  fIdentityForIdentity :: (Identity Int) -> Bool
+  fIdentityForIdentity x = functorIdentity x
+
+  fComposeForIdentity :: (Identity Int) -> IntInt -> IntInt -> Bool
+  fComposeForIdentity x (Fun _ f) (Fun _ g) = functorCompose f g x
+
+--   Functor for Pair a b
+
+  data Pair a = Pair a a deriving (Eq, Show)
+
+  instance Functor Pair where
+    fmap f (Pair a b) = Pair (f a) (f b)
+
+  instance (Arbitrary a) => Arbitrary (Pair a) where
+    arbitrary = do
+      a <- arbitrary
+      return (Pair a a)
+
+  fIdentityForPair :: (Pair Int) -> Bool
+  fIdentityForPair x = functorIdentity x
+
+  fComposeForPair :: (Pair Int) -> IntInt -> IntInt -> Bool
+  fComposeForPair x (Fun _ f) (Fun _ g) = functorCompose f g x
+
+--   Functor for Two
+
+  data Two a b = Two a b deriving (Eq, Show)
+
+  instance Functor (Two a) where
+    fmap f (Two a b) = Two a (f b)
+
+  instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+    arbitrary = do
+      a <- arbitrary
+      b <- arbitrary
+      return (Two a b)
+
+  functorIdentityForTwo :: (Two Int Int) -> Bool
+  functorIdentityForTwo x = functorIdentity x
+
+  fComposeForTwo :: (Two Int Int) -> IntInt -> IntInt -> Bool
+  fComposeForTwo x (Fun _ f) (Fun _ g) = functorCompose f g x
 
   chapter16Intermission :: IO ()
   chapter16Intermission = do
     quickCheck f
     quickCheck fcompose
     quickCheck fcompose'
+
+    quickCheck fIdentityForIdentity
+    quickCheck fComposeForIdentity
+
+    quickCheck fIdentityForPair
+    quickCheck fComposeForPair
+
+    quickCheck functorIdentityForTwo
+    quickCheck fComposeForTwo
+
 
