030ebc9a243789ce934b0356770fb880d5f27d22
scalolli <basavarajkalloli@gmail.com>
2018-05-10 06:14:31 +0100

    implemented infinte list

--- src/Chapter17/ZipListMonoid.hs
+++ src/Chapter17/ZipListMonoid.hs
@@ -25,25 +25,6 @@ module Chapter17.ZipListMonoid where
     y <*> Nil' = Nil'
     (Cons' f xs) <*> ys = (f <$> ys) <> (xs <*> ys)
 
-  instance Eq a => EqProp (List' a) where (=-=) = eq
-
-  instance Arbitrary a => Arbitrary (List' a) where
-    arbitrary = do
-      x <- arbitrary
-      y <- arbitrary
-      (elements [(Cons' x y) , Nil'])
-
-  newtype ZipList' a = ZipList' (List' a) deriving (Eq, Show)
-
-  instance Monoid (ZipList' a) where
-    mempty = ZipList' mempty
-
-    mappend (ZipList' Nil') (ZipList' Nil') = ZipList' Nil'
-    mappend (ZipList' Nil') x = ZipList' Nil'
-    mappend x (ZipList' Nil') = ZipList' Nil'
-    mappend (ZipList' (Cons' a b)) (ZipList' l) = ZipList' (Cons' a (mappend b l))
-
-
   append :: List' a -> List' a -> List' a
   append Nil' ys = ys
   append (Cons' x xs) ys = Cons' x (append xs ys)
@@ -57,6 +38,50 @@ module Chapter17.ZipListMonoid where
 
   flatMap :: (a -> List' b) -> List' a -> List' b
   flatMap f xs = concat' (fmap f xs)
+
+  instance Eq a => EqProp (List' a) where (=-=) = eq
+
+  instance Arbitrary a => Arbitrary (List' a) where
+    arbitrary = do
+      x <- arbitrary
+      y <- arbitrary
+      (elements [(Cons' x y) , Nil'])
+
+  newtype ZipList' a = ZipList' (List' a) deriving (Eq, Show)
+
+  takeList' :: Int -> ZipList' a -> ZipList' a
+  takeList' no (ZipList' xs) = ZipList' (go no xs)
+    where go :: Int -> List' a -> List' a
+          go n Nil'         = Nil'
+          go 0 (Cons' x xs) = Nil'
+          go n (Cons' x xs) = Cons' x (go (n-1) xs)
+
+  mkInfiniteList :: ZipList' Integer
+  mkInfiniteList = ZipList' go
+    where
+          go = Cons' 1 go
+
+  instance Monoid (ZipList' a) where
+    mempty = ZipList' mempty
+
+    mappend (ZipList' Nil') (ZipList' Nil') = ZipList' Nil'
+    mappend (ZipList' Nil') x = ZipList' Nil'
+    mappend x (ZipList' Nil') = ZipList' Nil'
+    mappend (ZipList' (Cons' a b)) (ZipList' l) = ZipList' (Cons' a (mappend b l))
+
+  instance Functor ZipList' where
+    fmap _ (ZipList' Nil') = ZipList' Nil'
+    fmap f (ZipList' (Cons' h t)) = ZipList' (Cons' (f h) (fmap f t))
+
+  instance Applicative ZipList' where
+    pure f                          = ZipList' (Cons' f Nil')
+    (ZipList' Nil') <*> xs          = ZipList' Nil'
+    xs <*> (ZipList' Nil')          = ZipList' Nil'
+    (ZipList' xs) <*> (ZipList' ys) = ZipList' (go xs ys)
+        where
+            go (Cons' f fs) (Cons' y ys) = Cons' (f y) (go fs ys)
+            go           _   Nil'        = Nil'
+            go           Nil' _          = Nil'
 
   chapter17Exercises :: IO ()
   chapter17Exercises = do
