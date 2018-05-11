c0c042f10ded3df92b682bf80c7343d58dd66115
scalolli <basavarajkalloli@gmail.com>
2018-05-11 10:16:14 +0100

    functor implementation for Validation applicative

---
+++ src/Chapter17/ValidationApplicative.hs
@@ -0,0 +1,30 @@
+module Chapter17.ValidationApplicative where
+
+  import Test.QuickCheck
+  import Test.QuickCheck.Classes
+  import Test.QuickCheck.Function
+  import Test.QuickCheck.Checkers
+  import Control.Applicative
+  import Data.Monoid
+
+  data Validation e a = MyFailure e | MySuccess a deriving (Eq, Show)
+
+  instance Functor (Validation e) where
+    fmap f (MySuccess a) = MySuccess (f a)
+    fmap f (MyFailure e) = MyFailure e
+
+  instance (Arbitrary a, Arbitrary e) => Arbitrary (Validation e a) where
+    arbitrary = do
+      x <- arbitrary
+      y <- arbitrary
+      (elements [MyFailure x, MySuccess y])
+
+  instance (Eq a, Eq e) => EqProp (Validation e a) where
+    (=-=) = eq
+
+  validationApplicativeTests :: IO ()
+  validationApplicativeTests = do
+    quickBatch (functor (undefined :: Validation String (String, String, Int)))
+
+
+
