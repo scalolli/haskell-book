f987f123d4ef9d4908d9dd5fbf9092114a0de2f7
scalolli <basavarajkalloli@gmail.com>
2018-05-19 09:04:38 +0100

    some more do syntax practice

--- src/Chapter18/Intermissions.hs
+++ src/Chapter18/Intermissions.hs
@@ -13,9 +13,36 @@ module Chapter18.Intermissions where
 
   sequencing' :: IO ()
   sequencing' =
-    putStrLn "Hello " *> putStrLn "World !"
+    putStrLn "Welcome to Haskell !"   >>
+    putStrLn "Please enter your name: "  >>
+    getLine >>=
+    (\name -> putStrLn $ "Welcome " ++ name ++ " to the wonderful world of Haskell !")
+
+  sequencingWithDoSyntax :: IO ()
+  sequencingWithDoSyntax = do
+    putStrLn "Welcome to Haskell !"
+    putStrLn "Please enter your name: "
+    name <- getLine
+    putStrLn $ "Welcome " ++ name ++ " to the wonderful world of Haskell !"
+
+  twiceWhenEven :: [Integer] -> [Integer]
+  twiceWhenEven xs = do
+    x <- xs
+    if(even x)
+     then [x*x, x*x]
+     else []
 
   binding :: IO ()
   binding = do
     getLine >>= putStrLn
+
+  buildTupleUsingApplicative :: Num a => Maybe (a, a, a)
+  buildTupleUsingApplicative = pure (,,) <*> Just 1 <*> Just 2 <*> Just 3
+
+  buildTupleUsingMonad :: Num a => Maybe (a, a, a)
+  buildTupleUsingMonad = do
+    a <- Just 1
+    b <- Just 2
+    c <- Just 3
+    return (a, b, c)
 
