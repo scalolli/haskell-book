c61dbd534f8a70657965b96e8ddbe641eef0d255
Basavaraj Kalloli <basavaraj.kalloli@gmail.com>
2018-05-30 07:28:44 +0100

    added quick check tests for my implementation

--- src/Chapter20/src/Exercises.hs
+++ src/Chapter20/src/Exercises.hs
@@ -1,6 +1,7 @@
 module Exercises where
 
   import Data.Monoid
+  import Test.QuickCheck
 
   mySum :: (Foldable t, Num a) => t a -> a
   mySum xs = getSum $ foldMap Sum xs
@@ -17,10 +18,35 @@ module Exercises where
   myMaximum :: (Foldable t, Ord a) => t a -> Maybe a
   myMaximum = foldr (compareMaybe (>)) Nothing
 
-
   compareMaybe :: Ord a => (a -> a -> Bool) -> a -> Maybe a -> Maybe a
   compareMaybe fn a b =
     case fmap (fn a) b of
       Just True -> Just a
       Just False -> b
       Nothing    -> Just a
+
+
+  myExists :: a -> Maybe a -> Bool
+  myExists = undefined
+
+
+-- try custom gen for verifying my elem
+--   customGen :: Gen (Integer, [Integer])
+--   customGen = do
+--     xs <- (arbitrary :: Gen [Integer])
+--     x <- (arbitrary :: Gen Integer)
+--     return (x `mod` ((length xs) - 1), xs)
+
+  chapter20Intermissions:: IO ()
+  chapter20Intermissions = do
+    quickCheck $ property (forAll (arbitrary :: Gen [Integer]) validateMySum)
+    quickCheck $ property (forAll (arbitrary :: Gen (Maybe Integer)) validateMySum)
+
+    quickCheck $ property (forAll (arbitrary :: Gen [Integer]) validateMyProduct)
+    quickCheck $ property (forAll (arbitrary :: Gen (Maybe Integer)) validateMyProduct)
+
+  validateMySum :: (Foldable t, Eq a, Num a) => t a -> Bool
+  validateMySum xs = sum xs == mySum xs
+
+  validateMyProduct :: (Foldable t, Eq a, Num a) => t a -> Bool
+  validateMyProduct xs = product xs == myProduct xs
