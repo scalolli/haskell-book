c5d48991e7ada0e843e40ecd6509d352fbc831c6
Basavaraj Kalloli <basavaraj.kalloli@springernature.com>
2018-06-26 07:07:40 +0100

    Or traversable

--- src/Chapter21/src/Exercises.hs
+++ src/Chapter21/src/Exercises.hs
@@ -20,4 +20,30 @@ module Exercises where
     traverse f (Two a b) = (Two a) <$> f b
 
 
+  data Or a b = First a | Second b deriving (Eq, Show)
+
+  instance (Monoid a, Monoid b) => Monoid (Or a b) where
+    mempty = Second mempty
+    First a `mappend` First b = First a
+    Second a `mappend` Second b = Second $ a `mappend` b
+    Second a `mappend` First b = First b
+    First a `mappend` Second b = First a
+
+  instance Functor (Or a) where
+    fmap f (First a) = First a
+    fmap f (Second b) = Second (f b)
+
+
+  instance Monoid a => Applicative (Or a) where
+    pure a = Second a
+    (First a) <*> (First b) = First (a `mappend` b)
+    (Second f) <*> (Second a) = Second (f a)
+
+  instance Foldable (Or a) where
+    foldMap f (First a) = mempty
+    foldMap f (Second b) = f b
+
+  instance Traversable (Or a) where
+    traverse _ (First a) = pure (First a)
+    traverse f (Second b) = Second <$> f b
 
