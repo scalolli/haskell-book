0912b1833cbc21d1e605a446c46eedbf507105a2
Basavaraj Kalloli <basavaraj.kalloli@springernature.com>
2018-06-28 07:28:00 +0100

    Chapter 21 exercises

--- src/Chapter21/src/Exercises.hs
+++ src/Chapter21/src/Exercises.hs
@@ -1,5 +1,10 @@
 module Exercises where
 
+  import Test.QuickCheck
+  import Test.QuickCheck.Classes
+  import Test.QuickCheck.Checkers
+  import Data.Semigroup
+
   data Two a b = Two a b deriving (Eq, Show)
 
   instance (Monoid a, Monoid b) => Monoid (Two a b) where
@@ -20,30 +25,72 @@ module Exercises where
     traverse f (Two a b) = (Two a) <$> f b
 
 
-  data Or a b = First a | Second b deriving (Eq, Show)
+  data Or a b = MyFirst a | MySecond b deriving (Eq, Show)
 
   instance (Monoid a, Monoid b) => Monoid (Or a b) where
-    mempty = Second mempty
-    First a `mappend` First b = First a
-    Second a `mappend` Second b = Second $ a `mappend` b
-    Second a `mappend` First b = First b
-    First a `mappend` Second b = First a
+    mempty = MySecond mempty
+    MyFirst a `mappend` MyFirst b = MyFirst a
+    MySecond a `mappend` MySecond b = MySecond $ a `mappend` b
+    MySecond a `mappend` MyFirst b = MyFirst b
+    MyFirst a `mappend` MySecond b = MyFirst a
 
   instance Functor (Or a) where
-    fmap f (First a) = First a
-    fmap f (Second b) = Second (f b)
+    fmap f (MyFirst a) = MyFirst a
+    fmap f (MySecond b) = MySecond (f b)
 
 
   instance Monoid a => Applicative (Or a) where
-    pure a = Second a
-    (First a) <*> (First b) = First (a `mappend` b)
-    (Second f) <*> (Second a) = Second (f a)
+    pure a = MySecond a
+    (MyFirst a) <*> (MyFirst b) = MyFirst (a `mappend` b)
+    (MySecond f) <*> (MySecond a) = MySecond (f a)
 
   instance Foldable (Or a) where
-    foldMap f (First a) = mempty
-    foldMap f (Second b) = f b
+    foldMap f (MyFirst a) = mempty
+    foldMap f (MySecond b) = f b
 
   instance Traversable (Or a) where
-    traverse _ (First a) = pure (First a)
-    traverse f (Second b) = Second <$> f b
+    traverse _ (MyFirst a) = pure (MyFirst a)
+    traverse f (MySecond b) = MySecond <$> f b
+
+
+  data Identity a = Identity a deriving (Eq, Ord, Show)
+
+  instance Monoid a => Monoid (Identity a) where
+    mempty = Identity mempty
+
+    mappend (Identity a) (Identity b) = Identity (a `mappend` b)
+
+
+  instance Functor Identity where
+    fmap f (Identity a) = Identity (f a)
+
+
+  instance Applicative Identity where
+    pure = Identity
+
+    (Identity f) <*> (Identity a) = Identity $ f a
+
+
+  instance Foldable Identity where
+    foldMap f (Identity a) = f a
+
+
+  instance Traversable Identity where
+    traverse f (Identity a) = Identity <$> f a
+
+
+  instance Arbitrary a => Arbitrary (Identity a) where
+    arbitrary = Identity <$> arbitrary
+
+  instance Eq a => EqProp (Identity a) where (=-=) = eq
+
+  instance Eq a => EqProp (Sum a) where (=-=) = eq
+
+  identityTrigger :: Identity (Int, Int,  Maybe (Sum Int))
+  identityTrigger = undefined
+
+  chapter21Exercises :: IO ()
+  chapter21Exercises = do
+    quickBatch (traversable identityTrigger)
+
 
